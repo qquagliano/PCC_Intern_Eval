2782e8600c8b49ea5fee096cbe8f03c7a191ba9e
Quinton Quagliano <qmq2@students.calvin.edu>
2021-08-06 22:50:12 -0400

    Added Example Data Generation R Code
    I added the complete R code used to generate the example data to be used in the web app

---
+++ Example_Data_Gen.R
@@ -0,0 +1,35 @@
+library(tidyr)
+library(lubridate)
+library(readxl)
+library(writexl)
+
+Psychometrist <- sample(x = c("AA", "AB", "AC", "AD"), size = 200, replace = TRUE)
+
+Age <- rnorm(n = 200, mean = 50, sd = 15) %>%
+       trunc()
+
+DATE <- rep(times = 10, x = c(20210101, 
+                  20210102, 
+                  20210103, 
+                  20210104, 
+                  20210105, 
+                  20210106, 
+                  20210107, 
+                  20210108, 
+                  20210109,
+                  20210110,
+                  20210111,
+                  20210112,
+                  20210113,
+                  20210114,
+                  20210115,
+                  20210116,
+                  20210117,
+                  20210118,
+                  20210119,
+                  20210120)) %>%
+        ymd()
+
+data <- tibble(DATE, Age, Psychometrist)
+
+write_xlsx(x = data, path = "Example_data.xlsx")
